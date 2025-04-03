996bbe860b42f896f2b452162e9b887451f4e8f7
Admin1 <jericoroi05@gmail.com>
2025-04-03 17:04:34 +0800

    asd

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ marital_status_options = {'Single': 0, 'Married': 1}
 education_options = {'Graduate': 0, 'Not Graduate': 1}
 employment_status_options = {'Unemployed': 0, 'Employed': 1}
 credit_history_options = {'No/Bad Credit History': 0, 'Good Credit History': 1}
-property_area_options = {'Rural': 0, 'Semiurban': 1, 'Urban': 2}
+property_area_options = {'Rural': 0, 'Urban': 1}
 
 # Create two columns for the input form
 col1, col2 = st.columns(2)
@@ -214,7 +214,7 @@ if submit_button:
         # Create columns for text and visualization
         res_col1, res_col2 = st.columns([3, 2])
         
-        with res_col1:
+        with res_col1: 
             threshold = 0.7  # Define your threshold
             
             if prediction == 1:
