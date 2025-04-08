965b999e76b9b6ade9134acc5d63efaa1f48e32b
Admin1 <jericoroi05@gmail.com>
2025-04-08 12:13:13 +0800

    adsdas

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,7 +98,7 @@ st.markdown("""
     }
     </style>
     <div class="title-container">
-        <h1 style='color: white;'>Loan Approval Prediction</h1>
+        <h1 style='color: white;'>Loan Approval Prediction1</h1>
         <p style='color: #aaa; margin-bottom: 0;'>Predict your loan approval chances with machine learning</p>
     </div>
 """, unsafe_allow_html=True)
@@ -325,6 +325,9 @@ if submit_button:
         st.markdown('</div>', unsafe_allow_html=True)
         
 print("hello")
+
+# Add space at the bottom
+st.markdown("<br><br><br>", unsafe_allow_html=True)
 
 # End of file
         
