76d0a9a9f6f16eddb6a3b4c9bf5ad42003ece6ae
Admin1 <jericoroi05@gmail.com>
2025-05-15 17:51:13 +0800

    asddas

--- print_model_features.py
+++
@@ -1,5 +0,0 @@
-import joblib
-
-model = joblib.load('SWIFT/Models/decision_tree_smote_model.joblib')
-print("Model expects these features (in order):")
-print(getattr(model, 'feature_names_in_', 'No feature_names_in_ attribute found')) 
